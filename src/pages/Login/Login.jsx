2f9f7313c58197f6ac6cfa9e0aa75ada40f7c905
nicogc001 <you@example.com>
2026-05-23 17:18:53 +0200

    diseño preliminar

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,9 @@
+function Login() {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <h1 className="text-4xl font-black">Login NiTanMal</h1>
+      </div>
+    );
+  }
+  
+  export default Login;
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,9 @@
+function Register() {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <h1 className="text-4xl font-black">Registro NiTanMal</h1>
+      </div>
+    );
+  }
+  
+  export default Register;
